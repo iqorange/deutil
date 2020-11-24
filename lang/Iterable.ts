5da692abcf818aae834673260c9f6269224cf75f
LiBoran <iqorange@yeah.net>
2020-11-24 08:35:42 +0800

    -m

--- lang/Iterable.ts
+++ lang/Iterable.ts
@@ -1,5 +1,5 @@
-import Iterator from '../Iterator'
-import Consumer from '../function/Consumer'
+import Iterator from '../util/Iterator'
+import Consumer from '../util/function/Consumer'
 export default interface Iterable<T> {
     Iterator: Iterator<T>;
     forEach(action: Consumer<T>): void;
