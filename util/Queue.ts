481c68ef2e15896f1f3480fe4e137c9fc948557c
LiBoran <iqorange@yeah.net>
2021-01-25 21:02:28 +0800

    queue

--- util/Queue.ts
+++ util/Queue.ts
@@ -2,5 +2,10 @@
 import Collection from './Collection.ts'
 
 export default interface Queue<E> extends Collection<E> {
-
+    add(e: E): boolean;
+    offer(e: E): boolean;
+    remove(): E;
+    poll(): E;
+    element(): E;
+    peek(): E;
 }
